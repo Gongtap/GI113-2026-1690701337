8a567c36c119949ef36114586da2051fd7c5d93a
Gongtap <gongtap.pana@bumail.net>
2026-09-21 16:38:27 +0700

    Lab06

---
+++ Lab06/Program.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * Student ID :1690701337
+ * Name       :Gongtap Panawas
+ * Section    :129B
+ * No.        :N/A
+ * Course     : GI113 Computer Programming (GI)
+ */
+namespace Lab06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int lives = 5;
+
+            //if (lives <= 0) // ในวงเล็บคือเงื่อนไขที่ต้องเป็นจริง
+            //{
+            //    // บล็อคของโค้ดที่จะทำงาน เมื่อเงื่อนไขเป็นจริง
+            //    Console.WriteLine("game over");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("continue to play!");
+            //}
+
+            //// เมื่อเงื่อนไขทำงานเสร็จแล้ว หรือ เงื่อนไขไม่ตรงเลยโค้ดทำงานต่อ
+            //    Console.WriteLine("continue to run");
+
+            //// int level = 10;
+
+            //bool isPoisoned = false;
+            //if (isPoisoned) { } // ได้เท็จ เพราะ isPoisoned = false
+            //if (!isPoisoned) { } // ได้จริง เพราะ isPoisoned = false
+            //bool hasKey = false;
+          
+
+            //Console.Write("Your level (1-99): ");
+            //bool ok = int.TryParse(Console.ReadLine(), out int level);
+
+            //if (!ok || level < 1 || level > 99)
+            //{
+            //    Console.WriteLine("Invalid Level Input.");
+            //}
+            //else if (level >= 10 && hasKey) // && และ กับ || หรือ
+            //{
+            //    Console.WriteLine("Boss floor unlocked.");
+            //}
+            //else if (level >= 5) // ต้องมี level 5 ขึ้นไป
+            //{
+            //    if (hasKey == true) // และต้องมีกุญแจ กุญแจเป็นจริง
+            //    {
+            //        Console.WriteLine("The door opens.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Locked. Find a key.");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The door stays shut.");
+            //}
+
+        }
+    }
+}
